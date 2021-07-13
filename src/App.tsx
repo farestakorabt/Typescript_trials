068fcd1a94bc38028d53ffa35d569f0aead7a9f1
farestakorabt <farestakorabt@gmail.com>
2021-07-13 20:20:15 +0200

    feat: react hooks typescript

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import UseStateComponent from "./UseStateComponent";
+import UseEffectComponent from "./UseEffectComponent";
+import UseContextComponent from "./UseContextComponent";
+import UseReducerComponent from "./UseReducerComponent"; 
+import UseRefComponent from "./UseRefComponent"; 
+import CustomHookComponent from "./CustomHookComponent"; 
+
+function App() {
+  return (
+    <div>
+      <h1>Custom Hook</h1>
+      <CustomHookComponent />
+      <h1>useRef</h1>
+      <UseRefComponent />
+      <h1>useReducer</h1>
+      <UseReducerComponent />
+      <h1>useContext</h1>
+      <UseContextComponent />
+      <h1>useEffect</h1>
+      <UseEffectComponent />
+      <h1>useState</h1>
+      <UseStateComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CustomHookComponent.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export interface Beverage {
+  name: string;
+  producerName: string;
+  beverageName: string;
+  beverageColor: string;
+  beverageStyle: string;
+  producerLocation: string;
+  abv: number;
+  ibu: number;
+  logo: string;
+  level: number;
+}
+
+function useFetchData<Payload>(url: string): {
+  data: Payload | null;
+  done: boolean;
+} {
+  const [data, dataSet] = useState<Payload | null>(null);
+  const [done, doneSet] = useState(false);
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((d: Payload) => {
+        dataSet(d);
+        doneSet(true);
+      });
+  }, [url]);
+  return {
+    data,
+    done,
+  };
+}
+
+function CustomHookComponent() {
+  const { data, done } = useFetchData<Beverage[]>("/hv-taplist.json");
+  return <div>{done && <img src={data![2].logo} alt="Beverage logo" />}</div>;
+}
+
+export default CustomHookComponent;
